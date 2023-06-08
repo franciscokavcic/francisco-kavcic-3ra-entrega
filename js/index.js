6a65674e732b886e2cc5363b180c48676a1b9c5b
franciscokavcic <fco.kavcic@gmail.com>
2023-06-08 09:06:40 -0300

    animaciones

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.sr =ScrollReveal();
+
+   sr.reveal('.contenedor-formulario',{
+    duration:5000,
+    origin: 'button',
+    distance:'-100px'
+   });
+   window.sr =ScrollReveal();
+
+   sr.reveal('.cards-main',{
+    duration:5000,
+    origin: 'lefth',
+    distance:'-100px'
+   });
